a5dd2f11916776ad21e6b311fb3624df1ff8680f
Nver Khachoyan <khachoyannver@gmail.com>
2025-02-26 15:33:12 -0800

    removed nested submodules

--- config/core/plugins.zsh
+++ config/core/plugins.zsh
@@ -1,8 +1,10 @@
-for dir in functions plugins; do
-    for file in "$ZSH_CONFIG/$dir"/*.zsh(N) "$ZSH_CONFIG/$dir"/*/*.plugin.zsh(N); do
-        source "$file"
-    done
+for file in "$ZSH_CONFIG/functions"/*.zsh(N); do
+    source "$file"
 done
+
+source "$DOTFILES/plugins/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "$DOTFILES/plugins/zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
+source "$DOTFILES/plugins/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
 ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
